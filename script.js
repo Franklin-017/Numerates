462c0655023089b3efb0056d362ff7c80e940ade
franklin <franklin@iamneo.ai>
2021-11-08 15:43:05 +0530

    Game end message

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const randomNumber = Math.floor(Math.random() * 100);
 var noOfAttempt = 10;
 document.getElementById('attempt').innerHTML = noOfAttempt;
+const container = document.getElementById('gameSection');
 
 
 function checkTheNumber() {
@@ -9,15 +10,22 @@ function checkTheNumber() {
   document.getElementById('attempt').innerHTML = noOfAttempt;
   document.getElementById("number").value = '';
 
-  if(inputedNumber == randomNumber) {
-    document.getElementById('message').innerHTML = "CONGRATS!! You Won the Game";
-  } else if(inputedNumber > randomNumber) {
-    document.getElementById('message').innerHTML = "Guessed number in large";
+  if(noOfAttempt == 0) {
+    container.innerHTML = `<div>
+                              <span class="emoji">&#128542;</span>
+                              <h3>Better luck next time</h3>
+                          </div>`;
+  } else if(inputedNumber == randomNumber) {
+    container.innerHTML = `<div>
+                              <span class="emoji">🎉</span>
+                              <h3>CONGRATS!! You have Won the Game</h3>
+                          </div>`;
+  } else if(inputedNumber > 100 || inputedNumber < 1) {
+    document.getElementById('message').innerHTML = "Enter number between 1 to 100 &#128532;";
+  }else if(inputedNumber > randomNumber) {
+    document.getElementById('message').innerHTML = "Guessed number in large &#128532;";
   } else if(inputedNumber < randomNumber) {
-    document.getElementById('message').innerHTML = "Guessed number in smaller";
-  } else if(noOfAttempt == 0) {
-    document.getElementById('message').innerHTML = "You Lost";
-    document.getElementById('btn').disabled  = true;
+    document.getElementById('message').innerHTML = "Guessed number in smaller &#128532;";
   } else {
     document.getElementById('message').innerHTML = "Try Again!!";
   }
